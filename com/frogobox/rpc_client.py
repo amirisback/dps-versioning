28981a9ee1ab65406192a025aab78852f1c64145
amirisback <faisalamircs@gmail.com>
2020-03-14 22:29:27 +0700

    add: Comment for code

--- com/frogobox/rpc_client.py
+++ com/frogobox/rpc_client.py
@@ -1,28 +1,34 @@
 # import library xmlrpc client karena akan digunakan rpc
 import xmlrpc.client
 
+# import config file
 from com.frogobox.config import *
 
+# buat stub/skeleton (proxy) pada client
 rpcServer = xmlrpc.client.ServerProxy(BASE_CONFIG_URL)
 
+# versi dari client
 version_client = BASE_VERSION_CLIENT
 
+# Menampilkan Versi Client
 print("Version Client \t: " + str(version_client))
 print("-- Checking Version From Server --")
-print("Version Server \t: " + str(rpcServer.get_version_update()))
+print("Version Server \t: " + str(rpcServer.get_version_update()))  # Menampilkan versi server
 print()
 print("Result : ")
 
 print("----------------------------------")
-if rpcServer.updated_version(version_client):
+if rpcServer.updated_version(version_client):  # Checking versi server dan client
     print("Latest Version Apps - No Need Update")
 else:
     inputUser = input("Apakah Ingin Memperbarui Version? (Y/N) ")
     if inputUser == 'y':
+        # Update versi client
         version_client = rpcServer.get_version_update()
         print("Thanks For Update")
         print("Version Client \t : " + str(version_client))
     elif inputUser == 'n':
+        # Tidak mengupdate versi Client
         print("Your version not updated")
         print("Version Client \t : " + str(version_client))
     else:
--- com/frogobox/rpc_server.py
+++ com/frogobox/rpc_server.py
@@ -8,17 +8,22 @@ from com.frogobox.config import *
 
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
+    # batasi pada path /RPC2 saja
     rpc_paths = (BASE_CONFIG_PATH)
 
 
+# Deklarasi versi server
 version_server = BASE_VERSION_SERVER
 
+# Menampilkan versi server
 print("Version Server \t: " + str(version_server))
 
+# buat server serta register fungsi register_introspection_functions()
 rpcServer = SimpleXMLRPCServer((BASE_CONFIG_IP_ADDRESS, BASE_CONFIG_PORT), requestHandler=RequestHandler)
 rpcServer.register_introspection_functions()
 
 
+# Fungsi untuk mengecek versi
 def updated_version(version_client):
     if version_client == version_server:
         return True
@@ -26,11 +31,14 @@ def updated_version(version_client):
         return False
 
 
+# Fungsi mengembalikan nilai versi server
 def get_version_update():
     return version_server
 
 
+# Registrasi fungsi
 rpcServer.register_function(updated_version, 'updated_version')
 rpcServer.register_function(get_version_update, 'get_version_update')
 
+# Jalankan server
 rpcServer.serve_forever()
